3b46719e41d4b86adef6bfdcb06942302071aafd
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2020-05-04 16:56:06 +0200

    Hello World

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+const helloWorld = (balise, text, id) => {
+  let container = document.createElement(balise);
+  let content = document.createTextNode(text);
+  container.appendChild(content);
+
+  let Div = document.querySelector(id)
+  document.body.insertBefore(container, Div);
+}
+
+helloWorld('div','Hello World','name');
